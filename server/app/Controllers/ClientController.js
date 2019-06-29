b1b559d3a342b707d77ee6d17903b1218a8f873d
elexfreeman <elextraza@gmail.com>
2019-06-29 16:51:11 +0400

    add ClientController

--- server/app/Controllers/ClientController.js
+++ server/app/Controllers/ClientController.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+class ClientControler {
+
+    constructor() { };
+
+    static async init(req) {
+        let self = new ClientControler();
+
+        return self;
+
+    }
+
+
+    list() {
+
+    }
+
+}
+
+/**
+ * list clients
+ */
 router.get('/client/list', async (req, res, next) => {
+    const self = ClientControler.init(req);
     res.json({ data: 22 })
 });
 
-
+/**
+ * get client by id
+ */
 router.get('/client/:id', async (req, res, next) => {
+    const self = ClientControler.init(req);
     res.json({ data: req.params.id })
 });
 
